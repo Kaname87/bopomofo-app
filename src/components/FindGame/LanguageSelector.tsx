3a3536301355ee83f9882b19a625cd4228a7718a
Kaname87 <kaname.nakanishi@bibo.com.ph>
2019-08-25 03:02:21 +0800

    Add zh lang

--- src/components/FindGame/LanguageSelector.tsx
+++ src/components/FindGame/LanguageSelector.tsx
@@ -5,30 +5,30 @@ import { useTranslation } from "react-i18next";
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
   const changeLanguage = (lng: string) => {
+    console.log(lng);
     i18n.changeLanguage(lng);
+  };
+  const langList = [
+    { code: "ja", label: "日本語" },
+    { code: "zh-hant", label: "中文(繁體)" },
+    { code: "zh-hans", label: "中文(简体)" },
+    { code: "en", label: "English" }
+  ];
+
+  const renderLangButtons = () => {
+    return langList.map(lang => (
+      <button
+        key={lang.code}
+        className={styles.languageSelector}
+        onClick={() => changeLanguage(lang.code)}
+      >
+        {lang.label}
+      </button>
+    ));
   };
 
   return (
-    <div className={styles.languageSelectorWrapper}>
-      <button
-        className={styles.languageSelector}
-        onClick={() => changeLanguage("en")}
-      >
-        en
-      </button>
-      <button
-        className={styles.languageSelector}
-        onClick={() => changeLanguage("ja")}
-      >
-        ja
-      </button>
-      <button
-        className={styles.languageSelector}
-        onClick={() => changeLanguage("ch")}
-      >
-        ch
-      </button>
-    </div>
+    <div className={styles.languageSelectorWrapper}>{renderLangButtons()}</div>
   );
 };
 export default LanguageSelector;
